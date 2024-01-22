982e3c12c9f28ad6ab71b33c078aa075439cb02e
David O'Gara <davidogara@hotmail.co.uk>
2024-01-22 12:53:16 +0000

    Add full_report view Also tweak bootsrap styling on index.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReportList.as_view(), name='home'),
+    path('<slug:slug>/', views.full_report, name='full_report'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Report
 
@@ -8,3 +8,27 @@ class ReportList(generic.ListView):
     queryset = Report.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+
+def full_report(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/full_report.html`
+    """
+
+    queryset = Report.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/full_report.html",
+        {"report": post},
+    )
