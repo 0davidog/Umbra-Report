a55147e70489017f1253e15f8199701cc4db22e8
David C. O'Gara <davidogara@hotmail.co.uk>
2024-02-28 13:56:38 +0000

    Add edits for validation

--- blog/admin.py
+++ blog/admin.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Report, Comment, About
 
-
-@admin.register(Report)
-class ReportAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
-
-
 # Register your models here.
-
+admin.site.register(Report)
 admin.site.register(Comment)
 admin.site.register(About)
