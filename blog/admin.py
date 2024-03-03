71bb9e7559b973444c78e4abbdab73770ec3c915
David C. O'Gara <davidogara@hotmail.co.uk>
2024-03-03 18:11:33 +0000

    Add Admin links to nav bar

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Report, Comment, About
 
 # Register your models here.
-admin.site.register(Report)
-admin.site.register(Comment)
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_on', 'approved', 'status']
+    list_filter = ['status', 'approved', 'author']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content', 'author', 'created_on', 'approved', 'post']
+    list_filter = ['approved', 'author', 'post']
+
+admin.site.register(Report, ReportAdmin)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(About)
